76318926bbd693c670a815f0877f3fe08da9471d
YukthikaDinakaran <yukthikadinakaran@gmail.com>
2025-06-30 10:22:49 +0530

    app added

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+#Modular Coding
+def main():
+    st.set_page_config("Information Retrival")
+    st.header("Information-Retrival-System")
+
+
+    if __name__ == "__main__":
+        main()
+
