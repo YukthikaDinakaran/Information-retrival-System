c6c5ad8a298d333de3bdd3732e0c661e83d7b4c6
YukthikaDinakaran <167126996+YukthikaDinakaran@users.noreply.github.com>
2025-07-01 10:54:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'Generative AI Project',
     version = '0.0.0',
     author = 'Yukthika',
-    author_email = 'yukthi1411@gmail.com',
+    author_email = 'yukthi1452@gmail.com',
     packages = find_packages(),
     install_requires = []
 )
